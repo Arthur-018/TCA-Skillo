3d3490716d94d1499fe2a33812797b35e5e2a0e7
Moisés Dutra Teixeira <moisesdutrateixeira@gmail.com>
2025-11-16 22:00:08 -0300

    22:00

--- src/main/java/com/google/genai/Financas/BancoMySQL/Declaracao.java
+++ src/main/java/com/google/genai/Financas/BancoMySQL/Declaracao.java
@@ -1,19 +1,87 @@
 package com.google.genai.Financas.BancoMySQL;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Declaracao {
+    private String comando;
 
-    public void salvarDados(String nome,String email,String senhaUsuario) {
-        String sql = "'" +  nome + "','" + email + "','" + senhaUsuario + "'";
+    public String getComando() {
+        return comando;
+    }
+
+    public void setComando(String comando) {
+        this.comando = comando;
+    }
+
+    public void CadastraUsuario(String nome, String email, String senha) {
+        nome = nome.trim().replaceAll("\\s+", " ");
+        if (nome.isEmpty()) {
+            System.out.println("O nome não pode ser vazio!");
+            return;
+        }
+
+        email = email.trim().replaceAll("\\s+", " ");
+        if (email.isEmpty()) {
+            System.out.println("O email não pode ser vazio!");
+            return;
+        }
+
+        senha = senha.replaceAll("\\s+", " ");
+        if (senha.trim().isEmpty()) {
+            System.out.println("A senha não pode ser vazia!");
+            return;
+        }
+
+        String sql = "'" + nome + "','" + email + "','" + senha + "'";
+
         try (Connection conexao = Conexao.ConectaBanco();
-        Statement statement = conexao.createStatement()) {
-            statement.executeUpdate("insert into usuario (nome,email,senha) values (" + sql + ")");
+             Statement declaracao = conexao.createStatement()) {
+
+            declaracao.executeUpdate("insert into usuario (nome,email,senha) values (" + sql + ")");
             System.out.println("Um novo usuario foi cadastrado");
+
+        } catch (SQLNonTransientConnectionException e) {
+            System.out.println("Não foi possível conectar ao banco de dados.");
+
+        } catch (SQLTimeoutException e) {
+            System.out.println("O banco de dados demorou muito para responder.");
+
+        } catch (SQLIntegrityConstraintViolationException e) {
+            System.out.println("O email já esta cadastrado.");
+
         } catch (SQLException e) {
-            System.out.println("Tivemos algum problema com o banco de dados:" + e);;
+            System.out.println("Tivemos algum problema com o banco de dados:" + e);
+        }
+    }
+
+    public void LogarUsuario(String email, String senha) {
+        try (Connection conexao = Conexao.ConectaBanco();
+             Statement declaracao = conexao.createStatement()) {
+
+            String sql = "SELECT * FROM usuario WHERE email = '" + email + "';";
+            ResultSet busca = declaracao.executeQuery(sql);
+
+            if (busca.next()) {
+                String emailUsuario = busca.getString("email");
+                String senhaUsuario = busca.getString("senha");
+
+                if (email.equals(emailUsuario) && senha.equals(senhaUsuario)) {
+                    System.out.println("Seja bem vindo " + busca.getString("nome"));
+                } else {
+                    System.out.println("Senha ou email incorretos!");
+                }
+            } else {
+                System.out.println("Senha ou email incorretos!");
+            }
+
+        } catch (SQLNonTransientConnectionException e) {
+                System.out.println("Não foi possível conectar ao banco de dados.");
+
+        } catch (SQLTimeoutException e) {
+            System.out.println("O banco de dados demorou muito para responder.");
+
+        } catch (SQLException e) {
+            System.out.println("Tivemos algum problema com o banco de dados: " + e);
         }
     }
 }
--- src/main/java/com/google/genai/Financas/BancoMySQL/MainTesteMySQL.java
+++ src/main/java/com/google/genai/Financas/BancoMySQL/MainTesteMySQL.java
@@ -7,13 +7,42 @@ public class MainTesteMySQL {
         Declaracao declaracao = new Declaracao();
         Scanner leitura = new Scanner(System.in);
 
-        System.out.println("Digite seu nome");
-        String nome = leitura.nextLine();
-        System.out.println("Digite seu email");
-        String email = leitura.nextLine();
-        System.out.println("Digite sua senha");
-        String senhaUsuario = leitura.nextLine();
+        int numero = 0;
 
-        declaracao.salvarDados(nome,email,senhaUsuario);
+        while (numero != 3) {
+            System.out.println("""
+                1 - Cadastrar novo usuario
+                2 - Logar
+                3 - sair
+                """);
+
+            int escolha = leitura.nextInt();
+            leitura.nextLine();
+
+            switch (escolha) {
+                case 1 -> {
+                    System.out.println("Digite seu nome");
+                    String nome = leitura.nextLine();
+                    System.out.println("Digite seu email");
+                    String email = leitura.nextLine();
+                    System.out.println("Digite sua senha");
+                    String senhaUsuario = leitura.nextLine();
+
+                    declaracao.CadastraUsuario(nome, email, senhaUsuario);
+                }
+                case 2 -> {
+                    System.out.println("Vamos fazer login?");
+                    System.out.println("Digite seu email");
+                    String email2 = leitura.nextLine();
+                    System.out.println("Digite sua senha");
+                    String senhaUsuario2 = leitura.nextLine();
+
+                    declaracao.LogarUsuario(email2, senhaUsuario2);
+                }
+                case 3 -> numero = 3;
+
+                default -> System.out.println("Opção inválida!");
+            }
+        }
     }
 }
